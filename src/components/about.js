ba791c7c9d054d9c555d4dd08500336d397393b9
John Rickard <hello@johnrickard.me>
2020-03-20 00:24:07 +0100

    Create About YAML and fetch with GraphQL
    Todo: about__tags (should also fetch YAML with GraphQL)

--- src/components/about.js
+++ src/components/about.js
@@ -1,17 +1,16 @@
 import React from 'react'
+import YAMLAbout from '../yaml/about.yaml'
 
 const About = () => {
   return (
     <div className="container__about">
       <div className="anchor">
         <a id="about"> </a>
-        <h1 className="about__header">ABOUT</h1>
+        <h1 className="about__header">{YAMLAbout.title}</h1>
       </div>
-      <div className="about__subheader">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </div>
+      <div className="about__subheader">{YAMLAbout.subheader}</div>
       <div className="about__text">
+        {YAMLAbout.text}
         <div className="about__tags">
           <i class="fas fa-medal" /> Tripadvisor Certificate of Excellence
           Winner
