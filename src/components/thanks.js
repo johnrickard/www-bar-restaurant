2fe7f7b9d421d104aacfd5c0c20a971967a507d3
John Rickard <hello@johnrickard.me>
2020-03-20 01:11:37 +0100

    Create Thanks YAML and fetch with GraphQL

--- src/components/thanks.js
+++ src/components/thanks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { Helmet } from 'react-helmet'
+import YAMLThanks from '../yaml/thanks.yaml'
 
 import '../styles/global.sass'
 import '../fonts/fontawesome/css/all.min.css'
@@ -14,12 +15,11 @@ export default class extends React.Component {
             {'body { background-image: none; background-color: #341817; }'}
           </style>
         </Helmet>
-        <h1 className="thanks__header">RESERVATION SENT</h1>
-        <div className="thanks__subheader">Thank you</div>
+        <h1 className="thanks__header">{YAMLThanks.title}</h1>
+        <div className="thanks__subheader">{YAMLThanks.subheader}</div>
         <Link to="/">
           <div className="thanks__text">
-            <i class="fas fa-arrow-circle-left"></i> Click here to go back to
-            the website
+            <i class={YAMLThanks.icon}></i> {YAMLThanks.iconText}
           </div>
         </Link>
       </div>
